832ee0eac5e92dd2b382e227efbeb8d593d3b1c9
lorena tamayo <cottalorena@gmail.com>
2023-02-11 10:18:12 +0100

    sobreescribir con diferentes parametros

--- src/company/HelloWorld.java
+++ src/company/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		//holaMundo();
-		//holaMundo();
+		holaMundo(4);
 		
 		//metodo con parametro
 		//holaMundo("lorena");
@@ -14,12 +14,16 @@ public class HelloWorld {
 		System.out.println(devuelveTexto());
 
 	}
+    //sobreescribir con diferentes argumentos
+	private static void holaMundo(int i) {
+		System.out.println("el numero es "+ i);
+		
+	}
 
 	//metodo con parametro
 	private static void holaMundo(String name) {
 		System.out.println("hola"+name ); 
-		
-		
+			
 	}
 
 	//ejemplo de una funcion y llamada
